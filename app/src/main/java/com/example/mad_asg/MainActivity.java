db86edc099e3f683a6533f53f993bfe592e0d83a
Xin Le Chong <s10221973@connect.np.edu.sg>
2023-06-12 23:02:02 +0800

    create button now works need to connect delete to database (deleting a task and then rerunning the code the task will appear agn which is problem!!!)

--- app/src/main/java/com/example/mad_asg/MainActivity.java
+++ app/src/main/java/com/example/mad_asg/MainActivity.java
@@ -3,21 +3,27 @@ package com.example.mad_asg;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements TaskAdapter.OnItemClickListener {
     private RecyclerView recyclerView;
     private TaskAdapter adapter;
     private List<Task> taskList;
+    private TaskDatabase taskDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,17 +37,11 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.OnIte
         adapter = new TaskAdapter(taskList, this);
         recyclerView.setAdapter(adapter);
 
-        // Example task data
-        Task task1 = new Task(1, "Pending", "Task 1", "Task 1 Description",
-                new Date(), "10:00 AM", "11:00 AM", 60,
-                "Type 1", "Repeat 1", 0, "", 1);
-        Task task2 = new Task(2, "Completed", "Task 2", "Task 2 Description",
-                new Date(), "12:00 PM", "1:00 PM", 60,
-                "Type 2", "Repeat 2", 0, "", 1);
+        // Initialize the database
+        taskDatabase = new TaskDatabase(this);
 
-        // Add tasks to the list
-        taskList.add(task1);
-        taskList.add(task2);
+        // Load tasks from the database
+        taskList.addAll(taskDatabase.getAllTasks());
         adapter.notifyDataSetChanged();
 
         // Example button actions
@@ -51,7 +51,7 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.OnIte
         createButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Code for creating a new task
+                showCreateTaskDialog();
             }
         });
 
@@ -69,6 +69,110 @@ public class MainActivity extends AppCompatActivity implements TaskAdapter.OnIte
     @Override
     public void onItemClick(int position) {
         adapter.setSelectedPosition(position);
+    }
+
+    private void showCreateTaskDialog() {
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View dialogView = inflater.inflate(R.layout.dialog_create_task, null);
+
+        final EditText taskNameEditText = dialogView.findViewById(R.id.taskNameEditText);
+        final EditText taskDescEditText = dialogView.findViewById(R.id.taskDescriptionEditText);
+        final EditText taskStartTimeEditText = dialogView.findViewById(R.id.taskStartTimeEditText);
+        final EditText taskEndTimeEditText = dialogView.findViewById(R.id.taskEndTimeEditText);
+        final EditText taskDurationEditText = dialogView.findViewById(R.id.taskDurationEditText);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Create Task");
+        builder.setView(dialogView);
+        builder.setPositiveButton("Create", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String taskName = taskNameEditText.getText().toString().trim();
+                String taskDesc = taskDescEditText.getText().toString().trim();
+                String taskStartTime = taskStartTimeEditText.getText().toString().trim();
+                String taskEndTime = taskEndTimeEditText.getText().toString().trim();
+                String taskDurationString = taskDurationEditText.getText().toString().trim();
+
+                // Validate user input
+                boolean isValidInput = true;
+                StringBuilder errorMessage = new StringBuilder("Invalid input. Please correct the following:");
+
+                if (taskName.isEmpty()) {
+                    isValidInput = false;
+                    errorMessage.append("\n- Task name is required");
+                }
+                if (taskDesc.isEmpty()) {
+                    isValidInput = false;
+                    errorMessage.append("\n- Task description is required");
+                }
+                if (taskStartTime.isEmpty()) {
+                    isValidInput = false;
+                    errorMessage.append("\n- Task start time is required");
+                } else {
+                    if (!isValidTimeFormat(taskStartTime)) {
+                        isValidInput = false;
+                        errorMessage.append("\n- Task start time is in an invalid format");
+                    }
+                }
+                if (taskEndTime.isEmpty()) {
+                    isValidInput = false;
+                    errorMessage.append("\n- Task end time is required");
+                } else {
+                    if (!isValidTimeFormat(taskEndTime)) {
+                        isValidInput = false;
+                        errorMessage.append("\n- Task end time is in an invalid format");
+                    }
+                }
+                if (taskDurationString.isEmpty()) {
+                    isValidInput = false;
+                    errorMessage.append("\n- Task duration is required");
+                } else {
+                    try {
+                        int taskDuration = Integer.parseInt(taskDurationString);
+                        if (taskDuration <= 0) {
+                            isValidInput = false;
+                            errorMessage.append("\n- Task duration should be a positive number");
+                        }
+                    } catch (NumberFormatException e) {
+                        isValidInput = false;
+                        errorMessage.append("\n- Task duration should be a valid number");
+                    }
+                }
+
+                if (isValidInput) {
+                    // Create a new Task object
+                    Task newTask = new Task(taskList.size() + 1, "Pending", taskName, taskDesc, new Date(),
+                            taskStartTime, taskEndTime, Integer.parseInt(taskDurationString), "Type", "Repeat", 0, "", 1);
+
+                    // Add the new task to the list and database
+                    taskList.add(newTask);
+                    taskDatabase.addTask(newTask);
+
+                    // Notify the adapter of the new task
+                    adapter.notifyItemInserted(taskList.size() - 1);
+                } else {
+                    // Display error message for invalid input
+                    AlertDialog.Builder errorDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+                    errorDialogBuilder.setTitle("Invalid Input");
+                    errorDialogBuilder.setMessage(errorMessage.toString());
+                    errorDialogBuilder.setPositiveButton("OK", null);
+                    errorDialogBuilder.create().show();
+                }
+            }
+        });
+        builder.setNegativeButton("Cancel", null);
+        builder.create().show();
+    }
+
+    private boolean isValidTimeFormat(String time) {
+        try {
+            SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm", Locale.getDefault());
+            timeFormat.setLenient(false);
+            timeFormat.parse(time);
+            return true;
+        } catch (ParseException e) {
+            return false;
+        }
     }
 
     private void showDeleteConfirmationDialog(final int position) {
---
+++ app/src/main/java/com/example/mad_asg/TaskDatabase.java
@@ -0,0 +1,127 @@
+package com.example.mad_asg;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class TaskDatabase extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "task_database";
+    private static final int DATABASE_VERSION = 1;
+    private static final String TABLE_NAME = "tasks";
+    private static final String COLUMN_ID = "id";
+    private static final String COLUMN_STATUS = "status";
+    private static final String COLUMN_TASK_NAME = "task_name";
+    private static final String COLUMN_TASK_DESC = "task_desc";
+    private static final String COLUMN_TASK_DATE = "task_date";
+    private static final String COLUMN_TASK_START_TIME = "task_start_time";
+    private static final String COLUMN_TASK_END_TIME = "task_end_time";
+    private static final String COLUMN_TASK_DURATION = "task_duration";
+    private static final String COLUMN_TASK_TYPE = "task_type";
+    private static final String COLUMN_REPEAT = "repeat";
+    private static final String COLUMN_RECURRING_ID = "recurring_id";
+    private static final String COLUMN_RECURRING_DURATION = "recurring_duration";
+    private static final String COLUMN_TASK_USER_ID = "task_user_id";
+
+    public TaskDatabase(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String createTableQuery = "CREATE TABLE " + TABLE_NAME + " (" +
+                COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                COLUMN_STATUS + " TEXT, " +
+                COLUMN_TASK_NAME + " TEXT, " +
+                COLUMN_TASK_DESC + " TEXT, " +
+                COLUMN_TASK_DATE + " INTEGER, " +
+                COLUMN_TASK_START_TIME + " TEXT, " +
+                COLUMN_TASK_END_TIME + " TEXT, " +
+                COLUMN_TASK_DURATION + " INTEGER, " +
+                COLUMN_TASK_TYPE + " TEXT, " +
+                COLUMN_REPEAT + " TEXT, " +
+                COLUMN_RECURRING_ID + " INTEGER, " +
+                COLUMN_RECURRING_DURATION + " TEXT, " +
+                COLUMN_TASK_USER_ID + " INTEGER" +
+                ")";
+        db.execSQL(createTableQuery);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String dropTableQuery = "DROP TABLE IF EXISTS " + TABLE_NAME;
+        db.execSQL(dropTableQuery);
+        onCreate(db);
+    }
+
+    public void addTask(Task task) {
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_STATUS, task.getStatus());
+        values.put(COLUMN_TASK_NAME, task.getTaskName());
+        values.put(COLUMN_TASK_DESC, task.getTaskDesc());
+        values.put(COLUMN_TASK_DATE, task.getTaskDate().getTime());
+        values.put(COLUMN_TASK_START_TIME, task.getTaskStartTime());
+        values.put(COLUMN_TASK_END_TIME, task.getTaskEndTime());
+        values.put(COLUMN_TASK_DURATION, task.getTaskDuration());
+        values.put(COLUMN_TASK_TYPE, task.getTaskType());
+        values.put(COLUMN_REPEAT, task.getRepeat());
+        values.put(COLUMN_RECURRING_ID, task.getRecurringId());
+        values.put(COLUMN_RECURRING_DURATION, task.getRecurringDuration());
+        values.put(COLUMN_TASK_USER_ID, task.getTaskUserID());
+        db.insert(TABLE_NAME, null, values);
+        db.close();
+    }
+
+    public List<Task> getAllTasks() {
+        List<Task> taskList = new ArrayList<>();
+        SQLiteDatabase db = getReadableDatabase();
+        Cursor cursor = db.query(TABLE_NAME, null, null, null, null, null, null);
+        if (cursor != null) {
+            while (cursor.moveToNext()) {
+                int idIndex = cursor.getColumnIndex(COLUMN_ID);
+                int statusIndex = cursor.getColumnIndex(COLUMN_STATUS);
+                int taskNameIndex = cursor.getColumnIndex(COLUMN_TASK_NAME);
+                int taskDescIndex = cursor.getColumnIndex(COLUMN_TASK_DESC);
+                int taskDateIndex = cursor.getColumnIndex(COLUMN_TASK_DATE);
+                int taskStartTimeIndex = cursor.getColumnIndex(COLUMN_TASK_START_TIME);
+                int taskEndTimeIndex = cursor.getColumnIndex(COLUMN_TASK_END_TIME);
+                int taskDurationIndex = cursor.getColumnIndex(COLUMN_TASK_DURATION);
+                int taskTypeIndex = cursor.getColumnIndex(COLUMN_TASK_TYPE);
+                int repeatIndex = cursor.getColumnIndex(COLUMN_REPEAT);
+                int recurringIdIndex = cursor.getColumnIndex(COLUMN_RECURRING_ID);
+                int recurringDurationIndex = cursor.getColumnIndex(COLUMN_RECURRING_DURATION);
+                int taskUserIdIndex = cursor.getColumnIndex(COLUMN_TASK_USER_ID);
+
+                int id = cursor.getInt(idIndex);
+                String status = cursor.getString(statusIndex);
+                String taskName = cursor.getString(taskNameIndex);
+                String taskDesc = cursor.getString(taskDescIndex);
+                long taskDateMillis = cursor.getLong(taskDateIndex);
+                Date taskDate = new Date(taskDateMillis);
+                String taskStartTime = cursor.getString(taskStartTimeIndex);
+                String taskEndTime = cursor.getString(taskEndTimeIndex);
+                long taskDuration = cursor.getLong(taskDurationIndex);
+                String taskType = cursor.getString(taskTypeIndex);
+                String repeat = cursor.getString(repeatIndex);
+                int recurringId = cursor.getInt(recurringIdIndex);
+                String recurringDuration = cursor.getString(recurringDurationIndex);
+                int taskUserID = cursor.getInt(taskUserIdIndex);
+
+                Task task = new Task(id, status, taskName, taskDesc, taskDate,
+                        taskStartTime, taskEndTime, taskDuration, taskType,
+                        repeat, recurringId, recurringDuration, taskUserID);
+                taskList.add(task);
+            }
+            cursor.close();
+        }
+        db.close();
+        return taskList;
+    }
+}
+
