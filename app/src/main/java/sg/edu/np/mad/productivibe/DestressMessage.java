d9c72a5ec714f90da2a033e6d0b43c3ad276f2fb
gwynethyeo723 <s10222843@connect.np.edu.sg>
2023-06-18 22:55:09 +0800

    gif finally moves!!

--- app/src/main/java/sg/edu/np/mad/productivibe/DestressMessage.java
+++ app/src/main/java/sg/edu/np/mad/productivibe/DestressMessage.java
@@ -7,25 +7,29 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 
 public class DestressMessage extends Fragment {
 
     View view;
+    ImageView gif;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_destress_message, container, false);
+        gif = view.findViewById(R.id.frog_gif);
+        // Adding the gif here using Glide library
+        Glide.with(this).asGif().load(R.drawable.frog).into(gif);
         return view;
     }
 
-    /*@Override View onCreate(Bundle saved instance)
 
-    ImageView gif = findViewById(R.id.gif);
 
-    // Adding the gif here using glide library
-        Glide.with(this).load(R.drawable.frog).into(gif);
-*/
+
+
+
 }
