9dbc114a5a6e75e9072f3f323ee3eb410ca7f6e1
gwynethyeo723 <s10222843@connect.np.edu.sg>
2023-07-08 22:53:46 +0800

    design for widget

--- app/src/main/java/sg/edu/np/mad/productivibe/TaskWidget.java
+++ app/src/main/java/sg/edu/np/mad/productivibe/TaskWidget.java
@@ -5,6 +5,7 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.widget.RemoteViews;
 
 /**
@@ -16,9 +17,11 @@ public class TaskWidget extends AppWidgetProvider {
                                 int appWidgetId) {
 
         // Construct the RemoteViews object
-        //int no = 25;
+        int no = 30;
+        String taskNo = no + " Tasks Due Today:";
         RemoteViews widgetViews = new RemoteViews(context.getPackageName(), R.layout.task_widget);
-        //widgetViews.setTextViewText(R.id.widgetTaskNo, String.valueOf(no));
+        widgetViews.setTextViewText(R.id.todayTask, taskNo);
+        Log.v("Widget", taskNo);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, widgetViews);
@@ -27,6 +30,7 @@ public class TaskWidget extends AppWidgetProvider {
         Intent intent = new Intent(context, HomePage.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
         widgetViews.setOnClickPendingIntent(R.id.todayTask, pendingIntent);
+        widgetViews.setOnClickPendingIntent(R.id.widgetTaskView, pendingIntent);
     }
 
     @Override
